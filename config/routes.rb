7af2556a3535f9f9b47cbf022cf8da9150a3e092
takumi <akaranki4@gmail.com>
2020-05-11 15:11:41 +0900

    投稿機能と各食堂の詳細ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "top#index"
   resources :top, only: :index
-  resources :cafeterias, only: [:index, :new]
+  resources :cafeterias
   # resources :users
 end
-
