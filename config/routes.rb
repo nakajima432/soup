ddc7721c900fc813b3e244b4d05c199f24fa24c3
takumi <akaranki4@gmail.com>
2020-05-11 09:24:34 +0900

    devise機能とtop画面の設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "cafeterias#index"
+  root to: "top#index"
+  resources :top, only: :index
+  resources :cafeterias, only: :index
   # resources :users
 end
