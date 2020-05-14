04e99915ec54a46d347cbe353554e455bc72e026
takumi <akaranki4@gmail.com>
2020-05-15 08:03:33 +0900

    コメント削除機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,12 @@ class CommentsController < ApplicationController
     redirect_to "/cafeterias/#{comment.cafeteria.id}"
   end
 
+  def destroy
+    @comment = Comment.find_by(id: params[:id],cafeteria_id: params[:cafeteria_id])
+    @comment.destroy
+    redirect_back(fallback_location: 'something')
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, cafeteria_id: params[:cafeteria_id])
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "top#index"
   resources :top, only: :index
   resources :cafeterias do
-    resources :comments, only: :create
+    resources :comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update] 
 end
