74bbfdeb4de93548fd945da2face4dcc7374cd94
takumi <akaranki4@gmail.com>
2020-05-17 12:52:12 +0900

    フォロー機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,29 @@ class User < ApplicationRecord
   has_many :liked_cafeterias, through: :likes, source: :cafeteria
   has_many :comments
   validates :username, :email, presence: true
+  
+  # ブックカフェアプリ
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following, through: :following_relationships
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships
+
+  def follow(other_user)
+    following_relationships.create!(following_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    following_relationships.find_by(following_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following_relationships.find_by(following_id: other_user.id)
+  end
+  
   def already_liked?(cafeteria)
     self.likes.exists?(cafeteria_id: cafeteria.id)
   end
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "top#index"
   resources :top, only: :index
+
   namespace :cafeterias do
     resources :searches, only: :index
   end
+
   resources :cafeterias do
     resources :comments, only: [:create, :destroy]
     resources :likes, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit, :update] 
+
+  resources :users, only: [:show, :edit, :update] do
+    member do
+      get :following, :followers
+    end
+  end
+  
+  resources :relationships, only: [:create, :destroy]
+
 end
