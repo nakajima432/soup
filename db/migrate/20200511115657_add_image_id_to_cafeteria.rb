f47d9d2bb531c1d2d5965b2ef3cc4a95349867b6
takumi <akaranki4@gmail.com>
2020-05-27 23:44:11 +0900

    マイグレーションの不具合を修正

--- db/migrate/20200511115657_add_image_id_to_cafeteria.rb
+++
@@ -1,5 +0,0 @@
-class AddImageIdToCafeterias < ActiveRecord::Migration[5.2]
-  def change
-    add_column :cafeterias, :image_id, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_16_144247) do
+ActiveRecord::Schema.define(version: 2020_05_27_144011) do
 
   create_table "cafeteria", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
