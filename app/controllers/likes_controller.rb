80136021eb8e3e1128535f0e8540644a30612d7d
takumi <akaranki4@gmail.com>
2020-06-07 09:48:53 +0900

    いいね機能をAjax化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,15 @@
 class LikesController < ApplicationController
+
   def create
-    @like = current_user.likes.create(cafeteria_id: params[:cafeteria_id])
-    redirect_back(fallback_location: root_path)
+    @cafeteria = Cafeteria.find(params[:cafeteria_id])
+    like = current_user.likes.create(cafeteria_id: params[:cafeteria_id])
+    like.save
   end
 
   def destroy
-    @like = Like.find_by(cafeteria_id: params[:cafeteria_id], user_id: current_user.id)
-    @like.destroy
-    redirect_back(fallback_location: root_path)
+    @cafeteria = Cafeteria.find(params[:cafeteria_id])
+    like = Like.find_by(cafeteria_id: params[:cafeteria_id], user_id: current_user.id)
+    like.destroy
   end
+
 end
