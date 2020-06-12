04d757b16766bb030b61f8de1e8892f61581f051
takumi <akaranki4@gmail.com>
2020-06-12 20:27:19 +0900

    スペルミスの修正

--- app/controllers/Users/registrations_controller.rb
+++ app/controllers/Users/registrations_controller.rb
@@ -47,7 +47,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :profile :profile_image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :profile, :profile_image])
   end
 
   # The path used after sign up.
